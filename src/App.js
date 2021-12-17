db389fa7ac462997b8bcd31d312dba22c9214a42
safiultarek143 <safiultarek143@gmail.com>
2021-12-18 03:05:06 +0600

    class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Functions from './components/Function';
+import Welcome from './Welcome';
 
 function App() {
   return (
     <div className="App">
       <Functions />
+      <Welcome />
     </div>
   );
 }
---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class Welcome extends Component{
+    render(){
+        return(
+            <h1>This is class component</h1>
+
+        )
+    
+        
+    }
+}
+export default Welcome
