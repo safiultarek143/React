ef0b54d350d86c09fa90ff21ef2ecc928192862e
safiultarek143 <safiultarek143@gmail.com>
2021-12-20 19:18:15 +0600

    jsx basic syntax

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Navbar =() =>{
-return (
-    <h3>this is navbar</h3>
-)
+// return (
+//     <h3>this is navbar</h3>
+// )
+    return React.createElement('div',{id:'nav-link', className:'nav-item'},React.createElement('h3',null,'This is navbar'))
 }
+export default Navbar
