0cdb2dd7d87e3e7d4870d200716c4b63fc4ca238
safiultarek143 <safiultarek143@gmail.com>
2021-12-27 20:04:06 +0600

    set state continue

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Functions from './components/Function';
 import Welcome from './Welcome';
 import Navbar from './components/Navbar';
 import Subscribe from './components/Subscribe';
+import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
       {/* <Functions name="jhon" age="40" >This is children props</Functions>
       <Functions name="rocky" age="20" />
       <Functions name= "jack" age="30" /> */}
-      <Subscribe />
+      {/* <Subscribe /> */}
       {/* <Welcome /> */}
+      <Counter />
     </div>
   );
 }
