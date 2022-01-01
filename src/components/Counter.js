4cb105f5c6e5c7e833a19403fbbb910522dfb253
safiultarek143 <safiultarek143@gmail.com>
2022-01-01 13:37:25 +0600

    counting 5 add

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -21,12 +21,19 @@ class Counter extends Component {
         )
         
     }
+    incrementFive(){
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment() 
+    }
     
     render() {
         return (
             <div>
                 <h1>Count: {this.state.count}</h1>
-                <button onClick={() => this.increment()}>Increment</button>
+                <button onClick={() => this.incrementFive()}>Increment</button>
             </div>
         )
     }
