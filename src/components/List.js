9c802ad3f3db9c9c6c3a5d74e5992659e5e322fc
safiultarek143 <safiultarek143@gmail.com>
2022-01-02 19:35:05 +0600

    listing with key prop

--- src/components/List.js
+++ src/components/List.js
@@ -1,22 +1,39 @@
 import React from 'react'
+import Person from './Person'
+
 
 const List = () => {
-    const persons =['Tarif','Sorif','Emon','joy','Israfil']
+    const persons =[{
+        id: 1,
+        name: "megh"
+    },
+    {
+        id: 2,
+        name: "jack"
+    },
+    {
+        id: 3,
+        name: "pack"
+    },
+    {
+        id: 4,
+        name: "nack"
+    },
+    ]
+    // const personList = persons.map((person) => <Person person={person} />)
     return (
-        // <div>
-        //     <h2>{persons[0]}</h2>
-        //     <h2>{persons[1]}</h2>
-        //     <h2>{persons[2]}</h2>
-        //     <h2>{persons[3]}</h2>
-        //     <h2>{persons[4]}</h2>
-        // </div>
-        // explicit return
+        
         <div>
             {
-                persons.map((person)=> <h2>{person}</h2>)
+              persons.map((person) => {
+                  return(
+                      <div key={person.id}>
+                          <Person person={person} />
+                      </div>
+                  )
+              })
             }
         </div>
-            // explicit return
     )
 }
 
---
+++ src/components/Person.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Person = ({person}) => {
+    return (
+        <div>
+            <h2>Hello, {person.name}</h2>
+        </div>
+    )
+}
+
+export default Person
